fe6c05cb8268e236e7442ac5de9e328fb25ea6ab
ShanCheng <351334650@qq.com>
2022-04-04 23:23:40 +0800

    hot-fix Second commit

--- src/main/java/com/java/springboot/git/GitTest.java
+++ src/main/java/com/java/springboot/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello,git---1");
         System.out.println("hello,git---2");
         System.out.println("hello,git---3");
+        System.out.println("hello,git---5");
     }
 }
