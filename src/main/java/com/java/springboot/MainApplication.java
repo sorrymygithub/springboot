10e075a2dc08812d17d1068c9e5e77162a1edf31
ShanCheng <351334650@qq.com>
2022-04-04 23:10:52 +0800

    third commit

--- src/main/java/com/java/springboot/MainApplication.java
+++ src/main/java/com/java/springboot/MainApplication.java
@@ -13,5 +13,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class,args);
+
     }
 }
